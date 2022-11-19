4c7b7437b0d690454f101ee68635706fac900c1f
James Mathew <jamesakm007@gmail.com>
2022-11-20 01:22:02 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,12 +1,11 @@
-# The following example shows how to issue an HTTP GET request supplying
-# an optional request header.
+#Issue an HTTP GET request to Terraform API
 data "http" "example" {
   url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
 
 }
 
+#Informations from Terraform API
 output "json_data" {
   value = jsondecode(data.http.example.response_body)
 }
-
 
