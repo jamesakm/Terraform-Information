704de30106ed461620b73cac36a7bc3bd089bd8a
James Mathew <jamesakm007@gmail.com>
2022-11-20 09:13:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
-#Module for Terraform API call
+// Module for Terraform API call
 module "terraform_info" {
 source = ".//module"
 }
 
-#Output only current Release, Version and Website details in JSON format
+// Output only current Release, Version and Website details in JSON format
 output "Terraform_information" {
   value = ({"Product"=module.terraform_info.all_todos.product,"Version"=module.terraform_info.all_todos.current_version,"Release"=module.terraform_info.all_todos.current_release,"Website"=module.terraform_info.all_todos.project_website})
 }
