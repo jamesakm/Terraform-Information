b8e171bc172ae881bf7512fa4b26d8d15515ff36
James Mathew <jamesakm007@gmail.com>
2022-11-20 09:11:19 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,11 +1,25 @@
-#Issue an HTTP GET request to Terraform API
-data "http" "example" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
-
+// Provider declaration - curl
+terraform {
+  required_providers {
+    curl = {
+      source = "anschoewe/curl"
+      version = "1.0.2"
+    }
+  }
 }
 
-#Informations from Terraform API
-output "json_data" {
-  value = jsondecode(data.http.example.response_body)
+// Accessing the Terraform API using GET
+data "curl" "getTodos" {
+  http_method = "GET"
+  uri = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+}
+
+locals {
+  json_data = jsondecode(data.curl.getTodos.response)
+}
+
+// Returns all JSON data
+output "all_todos" {
+  value = local.json_data
 }
 
